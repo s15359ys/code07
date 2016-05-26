46784fb92e965a2c90deb063f27d65e3e36c4202
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-05-26 10:13:34 +0900

    関数squareSumを定義
    関数squareSum:１から引数に指定された数までの二乗和を求める関数
    ただし、１より小さい数は引数に指定されない

--- js/sigma.js
+++ js/sigma.js
@@ -1,1 +1,14 @@
 const N10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function squareSum(n){
+if(n>1){
+ var a = 1;
+ var sum = 0;
+ while(a<n+1){
+ sum = sum + a*a;
+ a = a+1;
+}
+ return sum;
+}else{
+    alert(" １以上の数を入れてください");
+}
+}

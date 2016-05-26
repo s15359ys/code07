43332435ae2de3fd7dbd68898d7eb933587f8236
s15359ys <s15359ys@sfc.keio.ac.jp>
2016-05-26 10:20:44 +0900

    関数squareSum2を作成
    関数squareSum2:１より小さい数を引数に指定できるようにした改良版squareSum

--- js/sigma.js
+++ js/sigma.js
@@ -12,3 +12,22 @@ if(n>1){
     alert(" １以上の数を入れてください");
 }
 }
+function squareSum2(n){
+if(n>=1){
+ var a = 1;
+ var sum = 0;
+ while(a<n+1){
+ sum = sum + a*a;
+ a = a+1;
+}
+ return sum;
+}else if(n<1){
+ var a = 1;
+ var sum = 0;
+ while(a>n-1){
+ sum = sum + a*a;
+ a = a-1;
+}
+ return sum;
+}
+}
